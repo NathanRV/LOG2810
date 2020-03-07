cb46efdd1822c92f12bb9f72af04170f05f61aed
jefferson-lam <jefferson.lam@polymtl.ca>
2020-03-07 03:14:36 -0500

    Completed recursive function createGraph

--- src/TP1/Main.java
+++ src/TP1/Main.java
@@ -1,9 +1,27 @@
 package TP1;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello World!");
-        WeightedGraph w;
+        WeightedGraph w = new WeightedGraph();
+        List<String> graphInformation = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("resources/arrondissements.txt"))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                graphInformation.add(line);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        w.createGraph(graphInformation);
+        System.out.println("Success?");
     }
 }
