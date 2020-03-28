b5b39f357615fa0efe3403731c831241ce9e877f
ninalara <liliamnina@gmail.com>
2020-03-27 20:40:57 -0700

    interface added, exception handling needed (will do tmr)

--- src/TP1/Main.java
+++ src/TP1/Main.java
@@ -1,19 +1,41 @@
 package TP1;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException{
 
-        //w.traiterRequetes();
+        WeightedGraph w = new WeightedGraph();
+        Scanner read = new Scanner(System.in);
+        String menu = "Veuillez choisir parmi les options (a) a (d) : \n" +
+                      "(a) Mettre a jour la carte \n" +
+                      "(b) Determiner le plus court chemin securitaire \n" +
+                      "(c) Traiter les requêtes.\n" +
+                      "(d) Quitter \n";
+        System.out.println(menu);
+        String input = read.nextLine();
 
-        //TODO
-        Driver driver = new Driver("arrondissements.txt", "requetes.txt");
+        while (!input.equals("d")) {
+            w.createGraph("arrondissements.txt");
+            if (input.equals("a")) {
+                w.printGraph();
+            } else if (input.equals("b")) {
+                System.out.print("Veuillez entrer un point de depart : ");
+                int indexSource = read.nextInt();
+                System.out.print("Veuillez entrer un point de destination : ");
+                int indexDestination = read.nextInt();
+                w.printTrajectory(indexSource, indexDestination);
+            } else if (input.equals("c")) {
+                w.traiterRequetes();
+            } else {
+                System.out.println(menu + "Veuillez choisir une option entre (a) et (d) en saisissant la lettre correspondante. ex: a");
+            }
+            input = read.nextLine();
+        }
 
-        driver.doAll();
-
-
-        System.out.println();
-        System.out.println("15, 16, 7, 14, 4, 10, 12, 19, 17, 13, 8, 3, 19, 3, 5, 16, 7, 3, 12");
     }
+
+
 }
