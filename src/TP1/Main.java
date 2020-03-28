7c1b172cf335f4679715f3487be8b2e5cd916996
jefferson-lam <jefferson.lam@polymtl.ca>
2020-03-27 22:54:55 -0400

    Driver code

--- src/TP1/Main.java
+++ src/TP1/Main.java
@@ -7,11 +7,20 @@ import javax.swing.*;
 public class Main extends JFrame implements ActionListener {
 
     static JTextField sourceField, destinationField;
+    static JLabel informationOutput;
+
+    static WeightedGraph w = new WeightedGraph();
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        w.createGraph("arrondissements.txt");
+
         JFrame frame = new JFrame();
         frame.getContentPane().setLayout(new FlowLayout());
+
+        informationOutput = new JLabel("It is what it is");
+        informationOutput.setBounds(100, 100, 1000, 750);
+        frame.getContentPane().add(informationOutput);
 
         sourceField = new JTextField("", 10);
         destinationField = new JTextField("", 10);
@@ -26,6 +35,7 @@ public class Main extends JFrame implements ActionListener {
 
         sourceField.setBounds(100, 750, 150,40);
         destinationField.setBounds(260, 750, 150,40);
+
         closestPathButton.setBounds(440, 750, 150, 40);
 
         treatRequestsButton.setBounds(325, 850, 100, 40);
@@ -52,13 +62,6 @@ public class Main extends JFrame implements ActionListener {
 
         System.out.println();
         System.out.println("15, 16, 7, 14, 4, 10, 12, 19, 17, 13, 8, 3, 19, 3, 5, 16, 7, 3, 12");
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        String s = e.getActionCommand();
-        if (s.equals("Plus court chemin")) {
-
-        }
     }
 
 }
